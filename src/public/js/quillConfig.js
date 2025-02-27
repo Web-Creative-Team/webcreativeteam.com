84d1a986b043f9047a1c0509292803302653f63f
Petya Babukova <p_babukova@abv.bg>
2025-02-27 19:25:32 +0200

    Services section mobile styles amendments

--- src/public/js/quillConfig.js
+++ src/public/js/quillConfig.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     var editorContainer = document.querySelector('#editor-container');
     if (editorContainer) {
+        // 👉 Register Custom Size Class
+        var SizeClass = Quill.import('attributors/class/size');
+        SizeClass.whitelist = ['small', 'normal', 'large', 'huge'];
+        Quill.register(SizeClass, true);
+
         // 👉 Initialize Quill with enhanced toolbar options
         window.quill = new Quill('#editor-container', {
             theme: 'snow',
             modules: {
                 toolbar: [
                     [{ font: [] }],
-                    [{ size: ['small', 'normal', 'large', 'huge'] }],
+                    [{ size: [ 'normal', 'small', 'large', 'huge'] }],
                     [{ header: [1, 2, 3, 4, 5, 6, false] }],
                     ['bold', 'italic', 'underline', 'strike'],
                     [{ color: [] }, { background: [] }],
@@ -20,6 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             placeholder: 'Моля въведете съдържанието на статията тук...'
         });
+
+        // 👉 Set default font size to Normal
+        var defaultDelta = quill.clipboard.convert('<p class="ql-size-normal"><br></p>');
+        quill.setContents(defaultDelta);
 
         // 👉 Update hidden input before form submission
         var form = document.querySelector('form');
