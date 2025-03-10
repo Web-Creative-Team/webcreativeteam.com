9ae51123e1b13a7fc8ed7efd70f7e63b17aa49b6
Petya Babukova <p_babukova@abv.bg>
2025-03-10 16:15:46 +0200

    captcha tests

--- src/managers/emailManager.js
+++ src/managers/emailManager.js
@@ -7,6 +7,8 @@ const EMAIL_SERVICE = process.env.EMAIL_SERVICE;
 console.log("EMAIL:", process.env.EMAIL);
 console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD);
 console.log("EMAIL_SERVICE:", process.env.EMAIL_SERVICE);
+console.log("ENV:", process.env.NODE_ENV);
+console.log("EMAIL CONFIG:", process.env.EMAIL, process.env.EMAIL_SERVICE);
 
 
 const transporter = nodemailer.createTransport({
